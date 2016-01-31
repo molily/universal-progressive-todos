cd81343bb2f0046cdc725080c5ebf486d5ed5215
Mathias Schaefer (molily) <zapperlott@gmail.com>
2016-01-31 22:24:13 +0100

    Improved focussing

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -1,45 +1,64 @@
-import _ from 'lodash';
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import todoPropType from './todoPropType';
 import UpdateTodoForm from './UpdateTodoForm';
 
-const onSubmit = (props, event) => {
-  event.preventDefault();
-  const text = event.target.elements.text.value;
-  props.updateTodo({
-    ...props.todo,
-    text,
-    editMode: false
-  });
-};
+export default class EditTodoForm extends Component {
 
-const EditTodoForm = (props) => {
-  const newTodo = {
-    ...props.todo,
-    editMode: false
-  };
-  const fields = [ 'text' ];
-  return <UpdateTodoForm todo={newTodo} fields={fields}
-    onSubmit={_.partial(onSubmit, props)}>
-    <label>
-      <span className='accessible-hidden'>Edit todo:</span>
-      {/*
-      autofocus will not be in the server-rendered markup:
-      https://github.com/facebook/react/issues/3066
-      */}
-      <input type='text' name='text' defaultValue={newTodo.text}
-        placeholder='e.g., do the laundry' autoFocus
-        className='EditTodoForm__input'/>
-    </label>
-    <button type='submit' className='EditTodoForm__submitButton'>
-      💾 Save
-    </button>
-  </UpdateTodoForm>;
-};
+  constructor() {
+    super();
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.focusTextField();
+  }
+
+  componentDidUpdate() {
+    this.focusTextField();
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    const text = this.refs.text.value;
+    this.props.updateTodo({
+      ...this.props.todo,
+      text,
+      editMode: false
+    });
+  }
+
+  focusTextField() {
+    const { text } = this.refs;
+    text.select();
+    text.focus();
+  }
+
+  render() {
+    const newTodo = {
+      ...this.props.todo,
+      editMode: false
+    };
+    const fields = [ 'text' ];
+    return <UpdateTodoForm todo={newTodo} fields={fields}
+      onSubmit={this.onSubmit}>
+      <label>
+        <span className='accessible-hidden'>Edit todo:</span>
+        {/*
+        autofocus will not be in the server-rendered markup:
+        https://github.com/facebook/react/issues/3066
+        */}
+        <input ref='text' type='text' name='text' defaultValue={newTodo.text}
+          placeholder='e.g., do the laundry' className='EditTodoForm__input'/>
+      </label>
+      <button type='submit' className='EditTodoForm__submitButton'>
+        💾 Save
+      </button>
+    </UpdateTodoForm>;
+  }
+
+}
 
 EditTodoForm.propTypes = {
   todo: todoPropType,
   updateTodo: PropTypes.func.isRequired
 };
-
-export default EditTodoForm;
