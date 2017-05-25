f1d37b22fadbee67e71a8030c44fbbc31eaef9c9
Mathias Schaefer (molily) <zapperlott@gmail.com>
2017-05-26 00:54:15 +0200

    Simplify nesting again.

--- src/components/App.js
+++ src/components/App.js
@@ -3,28 +3,26 @@ import { h } from 'preact';
 import { Switch, Route, NavLink } from 'react-router-dom';
 import routes from '../routes';
 
-export default () => {
-  return (
-    <div>
-      <h1>Todos</h1>
-      <nav className='nav'>
-        <ul className='nav__list'>
-          <li className='nav__item'>
-            <NavLink to='/'>All</NavLink>
-          </li>
-          <li className='nav__item'>
-            <NavLink to='/active'>Active</NavLink>
-          </li>
-          <li className='nav__item'>
-            <NavLink to='/completed'>Completed</NavLink>
-          </li>
-        </ul>
-      </nav>
-      <Switch>
-        {routes.map((route, index) =>
-          <Route key={index} {...route} />
-        )}
-      </Switch>
-    </div>
-  );
-};
+export default () => (
+  <div>
+    <h1>Todos</h1>
+    <nav className='nav'>
+      <ul className='nav__list'>
+        <li className='nav__item'>
+          <NavLink to='/'>All</NavLink>
+        </li>
+        <li className='nav__item'>
+          <NavLink to='/active'>Active</NavLink>
+        </li>
+        <li className='nav__item'>
+          <NavLink to='/completed'>Completed</NavLink>
+        </li>
+      </ul>
+    </nav>
+    <Switch>
+      {routes.map((route, index) =>
+        <Route key={index} {...route} />
+      )}
+    </Switch>
+  </div>
+);
