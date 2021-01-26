ae96b90b6639ac98e4956ab6f0848ff7b2623aa4
Mathias Schaefer (molily) <molily@mailbox.org>
2021-01-26 22:39:03 +0100

    Simplify action type access. Simplify reducer.

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,7 +1,5 @@
-export default {
-  GET_TODOS: 'GET_TODOS',
-  CREATE_TODO: 'CREATE_TODO',
-  UPDATE_TODO: 'UPDATE_TODO',
-  DELETE_TODO: 'DELETE_TODO',
-  EDIT_TODO: 'EDIT_TODO',
-};
+export const GET_TODOS = 'GET_TODOS';
+export const CREATE_TODO = 'CREATE_TODO';
+export const UPDATE_TODO = 'UPDATE_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
+export const EDIT_TODO = 'EDIT_TODO';
